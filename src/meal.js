7ec373d011d26b7a4fc7193339c215a2329f7da0
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-07-08 08:47:42 -0600

    Started unit testing meal file

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
 
-}
+function nameMenuItem(item) {
+  return `Delicious ${item}`
+
+};
+
+function createMenuItem(name, price, type) {
+var menuItem = {
+  name,
+  price,
+  type,
+};
+return menuItem;
+};
+
+function addIngredients(ingredientName, ingredients) {
+for(var i = 0; i <= ingredients.length; i++ ) {
+  if(ingredientName == ingredients[i])
+  return;
+};
+ingredients[ingredients.length] = ingredientName;
+return ingredients;
+};
+
+function formatPrice (name, type, price) {
+  var menuItem = {
+    name,
+    price,
+    type,
+};
+return ("$" + menuItem.price);
+};
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
